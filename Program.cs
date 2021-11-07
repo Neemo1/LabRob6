da4ef4f499986ca19fea4ad3ff531d6323492dfd
ilia1 <ilia1@DESKTOP-0ETRE1V>
2021-11-07 18:57:35 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LabRob6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int sum = 0;
+            string k;
+            string strValue;
+            int[] iArray1 = new int[10];
+            int[] iArray2 = new int[10];
+            for (j = 0; j < 10; j++)
+            {
+                strValue = Console.ReadLine();
+                iArray1[j] = Convert.ToInt32(strValue);
+
+            }
+
+            for (j = 0; j < 10; j++) {
+                iArray2[j] = iArray1[j];
+                if (iArray2[j] < 0)
+                {
+                    iArray2[j] = iArray2[j] * (0 - 1);
+                   
+                }
+
+                sum += iArray2[j];
+            }
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("\n" + j + "    " + iArray1[j]);
+
+            }
+            Console.WriteLine("\n Сумма = " + sum + " Среднее = " + sum / 10d);
+        }
+    }
+}
